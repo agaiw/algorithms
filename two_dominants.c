86e2b3be52e5d779d70f1d6e8a74c18173ce1858
agaiw <iwaszkiewicz.aga@gmail.com>
2017-04-20 07:20:37 +0200

    dominants final version

--- two_dominants.c
+++ two_dominants.c
@@ -6,22 +6,26 @@ int main() {
   int testArray[] = {1, 4, 23, 98, 77, 60, 60, 34, 42, 86, 22, 21, 10, 94, 55, 55, 55, 60};
   int n = 18;               // size of testArray
   int max = 0;              // how many times dominant number(s) ocurr
-  int sort[LIMIT];      // cardinality array
+  int sort[LIMIT];          // cardinality array
 
+  // initializing cardinality table so it does not contain garbage
   for(int i = 0; i < LIMIT; i++) {
-    sort[i] = 0;            // uninitialized table contains garbage
+    sort[i] = 0;
   }
 
+  // filling cardinality table
   for(int i = 0; i < n; i++) {
     sort[testArray[i]]++;
   }
 
+  // finding dominant(s) cardinality
   for(int j = 0; j < LIMIT; j++) {
     if(max < sort[j]) {
       max = sort[j];
     }
   }
 
+  // printing out each dominant
   for(int k = 0; k < LIMIT; k++) {
     if(sort[k] == max) {
       printf("Number %d is one of most frequent numbers.\n", k);
