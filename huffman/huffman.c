bbc6a2df368d2257ad69c9ecf090c36f0a870e6b
agaiw <agnieszka.iwaszkiewicz@tieto.com>
2017-05-22 14:13:11 +0200

    corr

--- huffman/huffman.c
+++ huffman/huffman.c
@@ -6,6 +6,7 @@
 typedef struct node {
   char character;
   int frequency;
+  char* code;
   struct node* next_p;
   struct node* left;
   struct node* right;
@@ -17,32 +18,33 @@ void generateTree(node** start, const int freqSum);
 int sthToTreeize(node** start);
 void treeizeNodes(node** start, node** newNode, node **right, node **left);
 void findLowest(node** start, node** lowest);
-void calculateCodes(node** start, char* code);
+void calculateCodes(node** start, char** code);
 void findRoot(node** start, node** root);
 void freeList(node** start);
 
 int main (int argc, char* argv[]) {
+
   node* start = NULL;
-  char* input = "ala";
+  char* input = "In computer science and information theory, a Huffman code is a particular type of optimal prefix code that is commonly used for lossless data compression. The process of finding and/or using such a code proceeds by means of Huffman coding, an algorithm developed by David A. Huffman while he was a Sc.D. student at MIT, and published in the 1952 paper 'A Method for the Construction of Minimum-Redundancy Codes'. The output from Huffman's algorithm can be viewed as a variable-length code table for encoding a source symbol (such as a character in a file). The algorithm derives this table from the estimated probability or frequency of occurrence (weight) for each possible value of the source symbol. As in other entropy encoding methods, more common symbols are generally represented using fewer bits than less common symbols. Huffman's method can be efficiently implemented, finding a code in time linear to the number of input weights if these weights are sorted.[2] However, although optimal among methods encoding symbols separately, Huffman coding is not always optimal among all compression methods.";
   const int freqSum = strlen(input);
   calculateFrequencies(input, &start);
   generateTree(&start, freqSum);
   node* root = NULL;
   findRoot(&start, &root);
-
+  printf("Char\tCount\tCode\n");
   char* code0 = malloc(2 * sizeof(char));
   strcpy(code0, "0");
-  calculateCodes(&(root->left), code0);
+  calculateCodes(&(root->left), &code0);
   char* code1 = malloc(2 * sizeof(char));
   strcpy(code1, "1");
-  calculateCodes(&(root->right), code1);
+  calculateCodes(&(root->right), &code1);
  
   // test
   node* temp_p = start;
   while (temp_p != NULL) {
-    printf("ch: %c, fr: %d, addr: %p, next: %p, r: %p, l: %p, tree: %d\n", 
+    printf("ch: %c, fr: %d, code: %s, addr: %p, next: %p, r: %p, l: %p, tree: %d\n", 
            temp_p->character,
-           temp_p->frequency, temp_p, temp_p->next_p, temp_p->right,
+           temp_p->frequency, temp_p->code, temp_p, temp_p->next_p, temp_p->right,
            temp_p->left, temp_p->treeized);
     temp_p = temp_p->next_p;
   }
@@ -54,6 +56,9 @@ void freeList(node** start) {
   node* curr_p = *start;
   node* next_p = (*start)->next_p;
   while (next_p != NULL) {
+    if (curr_p->code != NULL) {
+      free(curr_p->code);
+    }
     free(curr_p);
     curr_p = next_p;
     next_p = next_p->next_p;
@@ -73,30 +78,30 @@ void findRoot(node** start, node** root) {
 }
 
 
-void calculateCodes(node** nd, char* code) {
+void calculateCodes(node** nd, char** code) {
   if ((*nd)->character != '\0') {
-    printf("character: %c, code: %s\n", (*nd)->character, code);
-    free(code);
+    (*nd)->code = *code;
+    printf("%c\t%d\t%s\n", (*nd)->character, (*nd)->frequency, *code);
   }
   else {
-    int len = strlen(code) + 2;
+    int len = strlen(*code) + 2;
     char* newCode0 = malloc(len * sizeof(char));
-    strcpy(newCode0, code);
+    strcpy(newCode0, *code);
     strcat(newCode0, "0");
-    calculateCodes(&((**nd).left), newCode0);
+    calculateCodes(&((**nd).left), &newCode0);
     char* newCode1 = malloc(len * sizeof(char));
-    strcpy(newCode1, code);
+    strcpy(newCode1, *code);
     strcat(newCode1, "1");
-    calculateCodes(&((**nd).right), newCode1);
-    free(code);
+    calculateCodes(&((**nd).right), &newCode1);
+    free(*code);
   }
 }
 
 void generateTree(node** start, const int freqSum) {
   while (sthToTreeize(start)) {
-    node* right = malloc(sizeof(node));
+    node* right = NULL;
     findLowest(start, &right);
-    node* left = malloc(sizeof(node));
+    node* left = NULL;
     findLowest(start, &left);
     node* newNode = malloc(sizeof(node));
     treeizeNodes(start, &newNode, &right, &left);
@@ -113,6 +118,7 @@ void treeizeNodes(node** start, node** newNode, node **right, node **left) {
   (*newNode)->character = '\0';
   (*newNode)->treeized = 0;
   (*newNode)->next_p = NULL;
+  (*newNode)->code = NULL;
   node* temp_p = *start;
   while (temp_p != NULL) {
     if (temp_p->next_p == NULL) {
@@ -161,6 +167,7 @@ void calculateFrequencies(char* input, node** start) {
   (*start)->right = NULL;
   (*start)->left = NULL;
   (*start)->treeized = 0;
+  (*start)->code = NULL;
   char_p++;
   // handling for rest of characters
   while(*char_p != '\0') {
@@ -180,6 +187,7 @@ void calculateFrequencies(char* input, node** start) {
         newNode->right = NULL;
         newNode->left = NULL;
         newNode->treeized = 0;
+        newNode->code = NULL;
         list_p->next_p = newNode;
         break;
       }
